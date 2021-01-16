111091ad63118c22dd0c7ab0a0883b6d26c972ce
Jianshuo Yang <jyang63@stevens.edu>
2021-01-15 19:47:10 -0500

    added redux middleware function and actionCreators.

--- react-redux-basics/src/index.js
+++ react-redux-basics/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -9,12 +9,26 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 const rootReducer = combineReducers({
-    ctr: counterReducer,
-    res: resultReducer
+  ctr: counterReducer,
+  res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] dispatching', action)
+      const res = next(action)
+      console.log('Middleware next state', store.getState())
+      return res
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
